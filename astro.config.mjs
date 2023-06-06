c9ed6ac0add8240120fe2c6f266d37619906edc8
shojihajime <shojihajime@shojihajime.local>
2023-06-06 18:07:54 +0900

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    define: {
+      'process.env.MICROCMS_SERVICE_DOMAIN': JSON.stringify(process.env.MICROCMS_SERVICE_DOMAIN),
+      'process.env.MICROCMS_API_KEY': JSON.stringify(process.env.MICROCMS_API_KEY)
+    }
+  },
+  output: "server",
+  adapter: cloudflare()
+});
